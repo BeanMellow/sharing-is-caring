961c3d170fc013a8590c864a86a5538280ec30f8
Filip Świętek <swietek.filip@gmail.com>
2019-01-30 23:33:30 +0100

    user registration with firebase

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './scss/main.scss';
 import App from './js/App';
+// import Firebase, { FirebaseContext } from './js/Firebase';
+import Firebase, { FirebaseContext } from './js/Firebase/firebaseIndex';
 
 document.addEventListener('DOMContentLoaded', function () {
-    ReactDOM.render(<App />, document.getElementById('app'));
+    ReactDOM.render(
+        <FirebaseContext.Provider value={new Firebase()}>
+            <App />
+        </FirebaseContext.Provider>,
+        document.getElementById('app'));
 });
--- src/js/Register.js
+++ src/js/Register.js
@@ -1,11 +1,110 @@
 import React from 'react';
+// import { FirebaseContext } from './Firebase/firebaseIndex';
+import { withFirebase } from './Firebase/firebaseIndex';
+import { Link } from 'react-router-dom';
 
-class Register extends React.Component {
+const INITIAL_STATE = {
+    username: '',
+    email: '',
+    passwordOne: '',
+    passwordTwo: '',
+    error: null
+};
+
+class RegisterFormBase extends React.Component {
+    state = { ...INITIAL_STATE };
+
+    handleChange = name => event => {
+        this.setState({
+            [name]: event.target.value
+        });
+    }
+
+    handleSubmit = event => {
+        const { username, email, passwordOne } = this.state;
+        event.preventDefault();
+        console.log('Submit');
+
+        this.props.firebase
+            .doCreateUserWithEmailAndPassword(email, passwordOne)
+            .then(authUser => {
+                this.setState({ ...INITIAL_STATE });
+            })
+            .catch(error => {
+                this.setState({ error });
+            });
+    }
+
     render() {
+        const isInvalid =
+            this.state.passwordOne !== this.state.passwordTwo ||
+            this.state.passwordOne === '' ||
+            this.state.email === '' ||
+            this.state.username === '';
+
         return (
-            <div>Register</div>
+            <div className='formContainer'>
+                <div className='mainContainer'>
+                    <form onSubmit={this.handleSubmit}>
+                        <label htmlFor='username'>Username</label>
+                        <input
+                            id='username'
+                            value={this.state.username}
+                            onChange={this.handleChange('username')}
+                            type='text'
+                            placeholder='Full name'
+                        />
+
+                        <label htmlFor='email'>Email</label>
+                        <input
+                            id='email'
+                            value={this.state.email}
+                            onChange={this.handleChange('email')}
+                            // type='email'
+                            type='text'
+                            placeholder='Email Address'
+                        />
+
+                        <label htmlFor='passwordOne'>Password</label>
+                        <input
+                            id='passwordOne'
+                            value={this.state.passwordOne}
+                            onChange={this.handleChange('passwordOne')}
+                            type='password'
+                            placeholder='Password'
+                        />
+
+                        <label htmlFor='passwordTwo'>Repeat password</label>
+                        <input
+                            id='passwordTwo'
+                            value={this.state.passwordTwo}
+                            onChange={this.handleChange('passwordTwo')}
+                            type='password'
+                            placeholder='Reapeat Password'
+                        />
+
+                        <button disabled={isInvalid} type='submit'>
+                            Sign up
+                        </button>
+                        {this.state.error && <p>{this.state.error.nessage}</p>}
+                        <p>Already got an account? <Link to='/login' className='link'>Sign in</Link></p>
+                    </form>
+                </div>
+            </div>
         );
     }
 }
+
+const RegisterForm = withFirebase(RegisterFormBase);
+
+const Register = () => (
+    <div>
+        <h2>Register</h2>
+        <RegisterForm />
+        {/* <FirebaseContext.Consumer>
+            {firebase => <RegisterForm firebase={firebase} />}
+        </FirebaseContext.Consumer> */}
+    </div>
+);
 
 export default Register;
